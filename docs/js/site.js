a52545f59ba45f6d22e3b243b81546c2817d5870
studydev <meelong0@gmail.com>
2026-06-05 00:22:41 +0900

    Clarity 스크립트 비동기 로드 기능 추가

--- docs/js/site.js
+++ docs/js/site.js
@@ -4,6 +4,15 @@
 (function () {
     'use strict';
 
+    (function (c, l, a, r, i, t, y) {
+        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = 'https://www.clarity.ms/tag/' + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+    })(window, document, 'clarity', 'script', 'x1tts8cy03');
+
     // ===== Sidebar toggle (mobile) =====
     var sidebar = document.getElementById('sidebar');
     var sidebarToggle = document.getElementById('sidebarToggle');
